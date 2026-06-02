ed1187b2da24d2b4b055d4ea200980b0d6fffbba
Bruno Braga <braga.bruno81@gmail.com>
2026-06-02 01:24:13 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.bragdev"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(26)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-redis-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+
+//implementation("org.flywaydb:flyway-core")
+//implementation("org.mapstruct:mapstruct")
+//implementation("io.github.resilience4j:resilience4j-spring-boot4")
+//implementation("org.springframework.retry:spring-retry")
+// Testing
+implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
+testImplementation("org.springframework.boot:spring-boot-starter-test")
+testImplementation("org.testcontainers:junit-jupiter")
+testImplementation("org.testcontainers:postgresql")
+testImplementation("org.testcontainers:kafka")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
